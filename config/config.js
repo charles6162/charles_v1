4ac91943727c210119bed431ae747fbf73e816d4
root <root@withim-105281.novalocal>
2021-11-07 12:58:10 +0900

    변경

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    MONGO_URL: 'mongodb://localhost:27017/SNS',
-    PORT:3000,
-    JWT_SECRET:'alfodptptwmdrnjsJWT',
-    EMAIL_USER:'kimsy31514@gmail.com',
-    EMAIL_PWD:'kim31514!',
-    NODE_ENV:'dev'
-}
